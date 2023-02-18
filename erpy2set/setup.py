504624299eb9d73a8d6532cb2acd99a6eca3c22f
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-18 13:11:56 -0600

    Create setup.py
    setting up erpy2set

---
+++ erpy2set/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='erpy2set',
+      version='1.0'
+      description'ergast-python-2-set'
+      author='Mauricio Carrillo'
+      author_email='mauricio.carrillo99@outlook.com"
+      packages=['erpy2set'],
+      install_requires=['numpy',
+                        'pandas',
+                        'requests',
+                        'json',
+                        'pdp']
