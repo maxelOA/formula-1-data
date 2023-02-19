9aa3cf3e12431795baf98d2380d731cbb5dd9749
Mauricio Carrillo <92446004+maxelOA@users.noreply.github.com>
2023-02-18 20:51:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 !pip install sys
 !pip install setuptools 
-!pip install numpy
-#python setup.py sdist bdist_wheel
+!pip install numpy as np
+!pip install requests as r
+!pip install pandas as pd
+!pip install json
 
+python setup.py sdist bdist_wheel
+
+import pandas
+import requests
+import json
+import numpy
 import sys
 import setuptools
 from setuptools import setup
 
 from setuptools import setup
 
-setup(
+setuptools.setup(
 	name='erpy2set',
 	version='1.0',
 	description='ergast-python-2-set',
